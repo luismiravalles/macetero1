291b21cfdfe12514f21f3ef9351323dce2bdb508
luis <luismiravalles@gmail.com>
2024-10-10 11:57:50 +0200

    Nextcloud funcionando

---
+++ conf/nextcloud.php
@@ -0,0 +1,41 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'upgrade.disable-web' => true,
+  'instanceid' => 'oc7zaevwdl08',
+  'passwordsalt' => 'YOIR1se4wohP1vjkDif2zXXdrlKSfm',
+  'secret' => '0e9reABY24MiZzhrFTgRt7reDqIfyAHu8jXPYprX+4CTd5+F',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost:9000',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '30.0.0.14',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'd1,hyi3kxG![NWcME6.MCaD75jy{yd',
+  'installed' => true,
+  'overwritewebroot' => '/nextcloud',
+  'overwrite.cli.url' => 'http://macetero.duckdns.org/nextcloud',
+);
