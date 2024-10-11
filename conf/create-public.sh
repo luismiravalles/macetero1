0bdd2359c400fe2c40bff84f906bb67d7915f8c0
luismiravalles <luismiravalles@gmail.com>
2024-10-11 09:05:13 +0200

    Nextcloud y carpeta publica funcionando!

---
+++ conf/create-public.sh
@@ -0,0 +1,7 @@
+#
+# Crear una carpeta publica enlazada con la carpeta publica de Nextcloud
+#
+# Ejecutar desde el directorio principal (macetero) con sudo
+#
+ln -s /var/snap/docker/common/var-lib-docker/volumes/macetero_nextcloud/_data/data/luis/files/Compartir/public ./public
+
