f7b77682b79dffa065e7f0a8f6c5c88439bbe312
Luis <luismiravalles@gmail.com>
2024-12-01 20:45:03 +0100

    Generar estilo csv

--- mp3app/src/main/java/com/miravalles/AbstractCommand.java
+++ mp3app/src/main/java/com/miravalles/AbstractCommand.java
@@ -7,7 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.function.Predicate;
+
+import org.apache.commons.lang3.StringUtils;
+
 
 /**
  * Clase base para heredar los diferentes comandos de ella.
@@ -16,10 +18,17 @@ public abstract class AbstractCommand {
 
     abstract public void run() throws Exception;
 
+    protected String getDir() {
+        return System.getProperty("dir");
+    }
+
+    protected String subpath(Path path) {
+        return StringUtils.removeStart(path.toString(), getDir());
+    }
 
 
     protected void walk(Predicado<Path> p) throws Exception {
-        Path directorio=Paths.get(System.getProperty("dir"));
+        Path directorio=Paths.get(getDir());
         Files.walkFileTree(directorio, new FileVisitor<Path>() {
             public FileVisitResult postVisitDirectory(Path path, java.io.IOException arg1) throws IOException { return FileVisitResult.CONTINUE;};
             public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes arg1) throws IOException  { return FileVisitResult.CONTINUE; };
--- mp3app/src/main/java/com/miravalles/FindSinTag.java
+++ mp3app/src/main/java/com/miravalles/FindSinTag.java
@@ -1,8 +1,11 @@
 package com.miravalles;
 
+import java.nio.file.Path;
+
 import org.apache.commons.lang3.StringUtils;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.mp3.MP3File;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
@@ -10,26 +13,40 @@ import org.jaudiotagger.tag.id3.AbstractID3v2Tag;
 
 public class FindSinTag extends AbstractCommand {
 
-    @Override
-    public void run() throws Exception {
-       walk( path -> {
-            AudioFile mp3=AudioFileIO.read(path.toFile());
-            Tag tag=mp3.getTag();
-            if(tag==null) {
-                System.out.println("--> " + path + ": No tiene TAG");
-                return true;
-            }
 
-            String titulo=tag.getFirst(FieldKey.TITLE);
-            if(StringUtils.isBlank(titulo)) {
-                System.out.println("-->" + path + ": No tiene título");
-                return true;
-            }
 
-            // System.out.println("---> El titulo de " + path + " es " + titulo);
+    private void report(Path path, String album, String artist, String title, String error) {
+        if(StringUtils.isBlank(album) || StringUtils.isBlank(artist) || StringUtils.isBlank(title)) {            
+            System.out.println(
+                subpath(path) + ";" + album + ";" + artist + ";" + title + ";" + error);
+        }
+    }
 
+    private boolean visit(Path path) throws Exception {
+        AudioFile mp3;
+        try {
+            mp3=AudioFileIO.read(path.toFile());
+        } catch(InvalidAudioFrameException e) {
+            report(path, "", "", "", e.getMessage());
             return true;
         }
-       );
+        Tag tag=mp3.getTag();
+        if(tag==null) {
+            report(path, "", "", "", "No tiene TAG");
+            return true;
+        }
+        String titulo=tag.getFirst(FieldKey.TITLE);
+        String album=tag.getFirst(FieldKey.ALBUM);
+        String artist=tag.getFirst(FieldKey.ARTIST);
+
+        report(path, album, artist, titulo, "");
+
+
+        return true;
+    }
+
+    @Override
+    public void run() throws Exception {
+       walk(this::visit);
     }
 }
